fb3f105a79d74f38009b40108c8546493326c240
unknown <jayrdoria876@gmail.com>
2023-06-23 02:04:52 +0800

    todo-quasarproject

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,11 @@
-<script setup>
-  
-  import {ref} from 'vue';
- // Variable Declaration
-  let todoList = ref([]);
-  let todo = ref(null);
-
-  // Adding a todo list
-  const addTodo = () => {
-    if (validateTodo(todo.value)) {
-      let id = todoList.value.length;
-      todoList.value.push({
-        id: ++id,
-        todo: todo.value,
-        is_done: false
-      })
-      todo.value = null
-    }
-  }
-
-  // Prevent duplicate todo
-  const validateTodo = (new_todo) => {
-    let index = todoList.value.findIndex((t) => t.todo === new_todo);
-    if (index !== -1) {
-      alert("Todo already exist!");
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  let selectedTodo = ref(null);
-  const selectTodo = (row) => {
-    selectedTodo.value = row;
-    todo.value = row.todo;
-  }
-
-  // Updating a todo from the list
-  const updateTodo = () => {
-    if (validateTodo(todo.value)) {
-      let index = todoList.value.findIndex((t) => t.id === selectedTodo.value.id);
-      index !== -1 && (todoList.value[index].todo = todo.value);
-
-
-      todo.value = selectedTodo.value = null;
-    }
-  }
-
-  // Marking to do as done
-  const markAsDone = (row) => {
-    if (confirm(`Are you sure you want to mark ${row.todo} as done?`)) {
-      let index = todoList.value.findIndex((t) => t.id === row.id);
-      index !== -1 && (todoList.value[index].is_done = true);
-    }
-  }
-
-  // Removing a todo from the list
-  const removeTodo = (row) => {
-    if (confirm(`Are you sure you want to delete ${row.todo}?`)) {
-      let index = todoList.value.findIndex((t) => t.id === row.id);
-      index !== -1 && todoList.value.splice(index, 1);
-    }
-  }
-</script>
-
-<!-- UI Design -->
 <template>
-  <div class="d-flex justify-content-center container">
-    <div class="w-75">
-        <form @submit.prevent="!selectedTodo ? addTodo() : updateTodo()">
-            <div class="mb-3 mt-5">
-              <label for="todo">What's your todo?</label>
-              <div class="d-flex">
-                <input v-model="todo" type="text" class="form-control" id="todo" placeholder="eg. Create todo application">
-                <!-- Identifying what button to appear -->
-                <button v-if="!selectedTodo" class="btn btn-primary ms-3"><i class="bi bi-plus-circle-fill"></i></button>
-                <button v-else class="btn btn-primary ms-3"><i class="bi bi-pencil-square"></i></button>
-              </div>
-            </div>
-        </form>
-        <!-- Rendering the to-do list using v-for -->
-        <div @click="selectTodo(row)" v-for="row in todoList" :key="row.id" class="toast show mt-2 w-100">
-          <div class="toast-header">
-            <strong class="me-auto">{{row.todo}}</strong>
-            <button :class="`btn btn-${row.is_done ? 'success' : 'outline-secondary'} btn-sm`" @click.stop="markAsDone(row)">
-              <i class="bi bi-check-lg"></i></button>
-              <button class="btn btn-danger btn-sm ms-1" @click.stop="removeTodo(row)">
-                <i class="bi bi-trash-fill"></i></button>
-          </div>
-        </div>
-    </div>
-  </div>
+  <router-view />
 </template>
 
-<style scoped>
+<script>
+import { defineComponent } from 'vue'
 
-</style>
+export default defineComponent({
+  name: 'App'
+})
+</script>
---
+++ src/composables/Tasks.js
@@ -0,0 +1,9 @@
+import {ref} from 'vue';
+
+
+let MyTasks = ref([])
+let FinishedTasks = ref([])
+let DeletedTasks = ref([])
+
+
+export { MyTasks, FinishedTasks, DeletedTasks };
